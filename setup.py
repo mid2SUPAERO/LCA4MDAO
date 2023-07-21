b025bbc11a231154fdac1b448baa1e33930da8b5
Thomas Bellier <thomas.bellier@isae-supaero.fr>
2023-07-21 20:27:15 +1000

    Created setup.py and package installation structure.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='LCA4MDAO',
+    version='0.1',
+    packages=['lca4mdao', 'lca4mdao.examples', 'lca4mdao.optimizer'],
+    url='https://github.com/mid2SUPAERO/LCA4MDAO',
+    license='MIT license',
+    author='Thomas Bellier',
+    author_email='thomas.bellier@isae-supaero.fr',
+    description=' Introducing Life Cycle Assessment in MDAO framework in an python environment. ',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
