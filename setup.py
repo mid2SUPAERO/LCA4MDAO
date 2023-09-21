6d7d280384dcd42064b38c1fda7b8801fa580609
Thomas Bellier <thomas.bellier@isae-supaero.fr>
2023-09-21 16:02:36 +1000

    Updated package setup and structure

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='LCA4MDAO',
     version='0.1',
     packages=['lca4mdao', 'lca4mdao.examples', 'lca4mdao.optimizer'],
+    include_package_data=True,
     url='https://github.com/mid2SUPAERO/LCA4MDAO',
     license='MIT license',
     author='Thomas Bellier',
